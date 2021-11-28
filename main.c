fea7c1a500f8f8f69f25fd0fd53f8f7ebea52b4b
mlebrun <mlebrun@student.42.fr>
2021-11-28 21:05:04 +0100

    feat: create squeleton

--- main.c
+++ main.c
@@ -6,13 +6,52 @@
 /*   By: mlebrun <mlebrun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 18:45:14 by mlebrun           #+#    #+#             */
-/*   Updated: 2021/11/28 18:48:57 by mlebrun          ###   ########.fr       */
+/*   Updated: 2021/11/28 20:18:20 by mlebrun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define		NB_CMD 2
+
+typedef struct		s_cmd
+{
+	char	*path;
+	int		pipe;
+	int		semicolon;
+}					t_cmd;
+
+void	wait_cmds_execution(int pids[NB_CMD])
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < NB_CMD)
+	{
+		waitpid(pids[i], NULL, 0);
+		i++;
+	}
+}
 
 int		main(int argc, char **argv, char **envp)
 {
+	int		i;
+	int		pids[NB_CMD];
+
+	i = 0;
+	while (i < NB_CMD)
+	{
+		pids[i] = fork();
+		if (pids[i] == 0)
+		{
+			return (0);
+		}
+		i++;
+	}
+	wait_cmds_execution(pids);
+	printf("parent\n");
 	return (0);
 }
